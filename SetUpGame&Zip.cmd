b823f9258ebf29a6b96d6486cc293e7f12252810
Jb645 <50148003+Jb645@users.noreply.github.com>
2022-03-05 14:44:38 -0500

    upgraded batch file

--- SetUpGame&Zip.cmd
+++ SetUpGame&Zip.cmd
@@ -1,17 +1,45 @@
 echo on
 Echo zipping...
-Set PackFilePath="C:\Program Files (x86)\Steam\steamapps\common\Quake 4\mod\game000.pk4"
-Set GameExePath="C:\Users\josue\OneDrive\Documents\Coding\GitHub\Win32\Release\Gamex86.dll"
-"C:\Program Files\7-Zip\7z.exe" a -tzip %PackFilePath% %GameExePath%
+Set PackFilePathLaptop="C:\Program Files (x86)\Steam\steamapps\common\Quake 4\mod\game000.pk4"
+Set PackFilePathDesktop="D:\SteamLibrary\steamapps\common\Quake 4\mod\game000.pk4"
+
+Set GameExePathLaptop="C:\Users\josue\OneDrive\Documents\Coding\GitHub\Win32\Release\Gamex86.dll"
+Set GameExePathDesktop="D:\GitHub\IT266-GameMod\Win32\Release\Gamex86.dll"
+
+if exist %PackFilePathLaptop% (
+echo Using Laptop!
+"C:\Program Files\7-Zip\7z.exe" a -tzip %PackFilePathLaptop% %GameExePathLaptop%
 echo Done!
+)
+
+if exist %PackFilePathDesktop% (
+echo Using Desktop!
+"D:\Programs\7-Zip\7z.exe" a -tzip %PackFilePathDesktop% %GameExePathDesktop%
+echo Done!
+)
+
 
 Echo deleting old File...
-Set NonZipDllPath="C:\Program Files (x86)\Steam\steamapps\common\Quake 4\mod\gamex86.dll"
-del %NonZipDllPath% /s /f /q
+Set NonZipDllPathLaptop="C:\Program Files (x86)\Steam\steamapps\common\Quake 4\mod\gamex86.dll"
+Set NonZipDllPathDesktop="D:\SteamLibrary\steamapps\common\Quake 4\mod\gamex86.dll"
+
+if exist %NonZipDllPathLaptop% (
+del %NonZipDllPathLaptop% /s /f /q
 echo Done!
+)
+
+if exist %NonZipDllPathDesktop% (
+del %NonZipDllPathDesktop% /s /f /q
+echo Done!
+)
 
 Echo Starting Mod
-Set ShortcutPath="Q4Mod"
+Set ShortcutPathLaptop="Q4Mod"
+Set ShortcutPathDesktop="D:\GitHub\IT266-GameMod\Game-Mod-Q4\Q4ModDesktop"
 
-start "" %ShortcutPath%
-echo Done!
+if exist %PackFilePathLaptop% (
+start "" %ShortcutPathLaptop%
+) 
+if exist %PackFilePathDesktop% (
+start "" %ShortcutPathDesktop%
+)
